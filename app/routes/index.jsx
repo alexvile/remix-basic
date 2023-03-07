d2694b2584f56dfa6b3c9f775ae51a3bb8f08156
Alexei Solotvinskiy <alexei20293@yahoo.com>
2023-03-07 17:49:08 +0200

    started project

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -1,32 +1,12 @@
+import { Link } from '@remix-run/react'
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-    </div>
+  <main id="content">
+    <h1 className="text-3xl font-bold underline">A better way to keeping notes</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+    <p id="cta">
+      <Link to="/notes" className='text-blue-400'> Try now !</Link>
+    </p>
+  </main>
   );
 }
---
+++ app/routes/notes.jsx
@@ -0,0 +1,6 @@
+export default function NotesPage() {
+    return(<main>
+        <h1>My notes</h1>
+    
+    </main>)
+}
