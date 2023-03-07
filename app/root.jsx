52d3a0b6b180fe8e0d67c1ad37433aaccda9ebad
Alexei Solotvinskiy <alexei20293@yahoo.com>
2023-03-07 17:48:52 +0200

    installed tailwind.css

--- app/root.jsx
+++ app/root.jsx
@@ -1,3 +1,5 @@
+import stylesheet from "~/tailwind.css";
+
 import {
   Links,
   LiveReload,
@@ -13,6 +15,9 @@ export const meta = () => ({
   viewport: "width=device-width,initial-scale=1",
 });
 
+export const links = () => [
+  { rel: "stylesheet", href: stylesheet },
+];
 export default function App() {
   return (
     <html lang="en">
